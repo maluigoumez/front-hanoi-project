96d7fe97ea5c36b89d77bb0aff4652b8447dacdb
Malui Gomez <luisagomezrodela@gmail.com>
2024-03-20 21:11:14 -0600

    Control buttons and style CSS added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Header from "./header.js"; // Change the import statement
+import Header from "./Header.js"; // Change the import statement to match your file structure
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="controls">
+        <input type="number" placeholder="Number of disks" />
+        <button>See movements</button>
+      </div>
     </div>
   );
 }
